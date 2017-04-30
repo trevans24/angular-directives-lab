d8bcab8e11408c62b7b88aad746c6bfef536e3e2
trevans24 <trevans24@gmail.com>
2017-04-30 13:59:27 -0600

    express routes

--- starter-code/server.js
+++ starter-code/server.js
@@ -16,6 +16,43 @@ app.get('/', function home(req, res){
 	res.sendFile(__dirname + '/views/index.html');
 });
 
+// Require DB models
+var db = require('./models');
+
+// GET
+app.get('/api/cards', (req,res)=>{
+	db.Card.find()
+	.exec((err, cards) => {
+		if (err){
+			console.log("Get error: ",err);
+		}
+		res.json(cards);
+	});
+});
+
+// SHOW
+app.get('/api/cards/:id', (req,res)=>{
+	db.Card.findOne({_id: req.params.id}, (err, data) =>{
+		if (err){
+			console.log("Show error: ",err);
+		}
+		res.json(data);
+	});
+});
+
+// POST
+app.post('/api/cards', (req,res)=>{
+	var newCard = new db.Card({
+		question: req.body.question
+	});
+	newCard.save((err,card) =>{
+		if (err){
+			console.log("Save Error: ", err);
+		}
+		res.json(card);
+	});
+});
+
 // server
 app.listen(3000, () => {
 	console.log('Server running on PORT 3000');
